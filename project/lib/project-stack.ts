3ff2993952c5f5ff69fbcb239645d10b575b8d3b
akmalpopalzi98 <105284469+akmalpopalzi98@users.noreply.github.com>
2023-05-17 15:40:39 +0100

    adding change

--- project/lib/project-stack.ts
+++ project/lib/project-stack.ts
@@ -16,7 +16,7 @@ export class ProjectStack extends cdk.Stack {
       crossAccountKeys: false,
       synth: new ShellStep("Synth", {
         input: CodePipelineSource.gitHub("akmalpopalzi98/awsPipeline", "main"),
-        commands: ['cdk synth'],
+        commands: ["cd project", "cdk synth"],
       }),
     });
   }
